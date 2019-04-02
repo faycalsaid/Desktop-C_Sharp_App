9bc2f5690692ca9dd6a2bc72424f43a97ca0efb2
feycce <45202367+feycce@users.noreply.github.com>
2019-04-02 13:41:10 +0200

    Bunifu and Test Push

--- Main/Main/Form1.Designer.cs
+++ Main/Main/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.bunifuCustomDataGrid1 = new Bunifu.Framework.UI.BunifuCustomDataGrid();
+            this.NouneGridView = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastNameGridView = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bunifuVTrackbar1 = new Bunifu.Framework.UI.BunifuVTrackbar();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuCustomDataGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuCustomDataGrid1
+            // 
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.bunifuCustomDataGrid1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.bunifuCustomDataGrid1.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.bunifuCustomDataGrid1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.bunifuCustomDataGrid1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.SeaGreen;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.SeaGreen;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.bunifuCustomDataGrid1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.bunifuCustomDataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.bunifuCustomDataGrid1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NouneGridView,
+            this.LastNameGridView});
+            this.bunifuCustomDataGrid1.DoubleBuffered = true;
+            this.bunifuCustomDataGrid1.EnableHeadersVisualStyles = false;
+            this.bunifuCustomDataGrid1.HeaderBgColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCustomDataGrid1.HeaderForeColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCustomDataGrid1.Location = new System.Drawing.Point(196, 81);
+            this.bunifuCustomDataGrid1.Name = "bunifuCustomDataGrid1";
+            this.bunifuCustomDataGrid1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.bunifuCustomDataGrid1.Size = new System.Drawing.Size(392, 164);
+            this.bunifuCustomDataGrid1.TabIndex = 0;
+            // 
+            // NouneGridView
+            // 
+            this.NouneGridView.HeaderText = "Nom";
+            this.NouneGridView.Name = "NouneGridView";
+            // 
+            // LastNameGridView
+            // 
+            this.LastNameGridView.HeaderText = "Nom";
+            this.LastNameGridView.Name = "LastNameGridView";
+            // 
+            // bunifuVTrackbar1
+            // 
+            this.bunifuVTrackbar1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuVTrackbar1.BackgroudColor = System.Drawing.Color.DarkGray;
+            this.bunifuVTrackbar1.BorderRadius = 0;
+            this.bunifuVTrackbar1.IndicatorColor = System.Drawing.Color.SeaGreen;
+            this.bunifuVTrackbar1.Location = new System.Drawing.Point(51, 105);
+            this.bunifuVTrackbar1.MaximumValue = 100;
+            this.bunifuVTrackbar1.Name = "bunifuVTrackbar1";
+            this.bunifuVTrackbar1.Size = new System.Drawing.Size(30, 198);
+            this.bunifuVTrackbar1.SliderRadius = 0;
+            this.bunifuVTrackbar1.TabIndex = 1;
+            this.bunifuVTrackbar1.Value = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 360);
+            this.Controls.Add(this.bunifuVTrackbar1);
+            this.Controls.Add(this.bunifuCustomDataGrid1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuCustomDataGrid1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuCustomDataGrid bunifuCustomDataGrid1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NouneGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastNameGridView;
+        private Bunifu.Framework.UI.BunifuVTrackbar bunifuVTrackbar1;
     }
 }
 
