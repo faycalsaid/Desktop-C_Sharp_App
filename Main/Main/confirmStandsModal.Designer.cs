3f30f1d3e36fdae1b3ef6bd2c05e813646d37dc0
Faycal <faycalhaboo@gmail.com>
2019-06-01 12:19:12 +0200

    -Merged Ateliers

--- Main/Main/confirmStandsModal.Designer.cs
+++ Main/Main/confirmStandsModal.Designer.cs
@@ -30,14 +30,17 @@ namespace Main
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(confirmStandsModal));
             this.areaTxtBox = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.localisationTxtBox = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.bunifuThinButton21 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.cBoxPartners = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // areaTxtBox
             // 
             this.areaTxtBox.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.areaTxtBox.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.areaTxtBox.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.areaTxtBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.areaTxtBox.HintForeColor = System.Drawing.Color.Empty;
             this.areaTxtBox.HintText = "";
@@ -45,18 +48,18 @@ namespace Main
             this.areaTxtBox.LineFocusedColor = System.Drawing.Color.Blue;
             this.areaTxtBox.LineIdleColor = System.Drawing.Color.Gray;
             this.areaTxtBox.LineMouseHoverColor = System.Drawing.Color.Blue;
-            this.areaTxtBox.LineThickness = 3;
+            this.areaTxtBox.LineThickness = 6;
             this.areaTxtBox.Location = new System.Drawing.Point(298, 177);
             this.areaTxtBox.Margin = new System.Windows.Forms.Padding(4);
             this.areaTxtBox.Name = "areaTxtBox";
-            this.areaTxtBox.Size = new System.Drawing.Size(370, 44);
+            this.areaTxtBox.Size = new System.Drawing.Size(106, 46);
             this.areaTxtBox.TabIndex = 1;
             this.areaTxtBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
             // localisationTxtBox
             // 
             this.localisationTxtBox.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.localisationTxtBox.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.localisationTxtBox.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.localisationTxtBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.localisationTxtBox.HintForeColor = System.Drawing.Color.Empty;
             this.localisationTxtBox.HintText = "";
@@ -64,13 +67,47 @@ namespace Main
             this.localisationTxtBox.LineFocusedColor = System.Drawing.Color.Blue;
             this.localisationTxtBox.LineIdleColor = System.Drawing.Color.Gray;
             this.localisationTxtBox.LineMouseHoverColor = System.Drawing.Color.Blue;
-            this.localisationTxtBox.LineThickness = 3;
+            this.localisationTxtBox.LineThickness = 6;
             this.localisationTxtBox.Location = new System.Drawing.Point(298, 87);
             this.localisationTxtBox.Margin = new System.Windows.Forms.Padding(4);
             this.localisationTxtBox.Name = "localisationTxtBox";
-            this.localisationTxtBox.Size = new System.Drawing.Size(370, 44);
+            this.localisationTxtBox.Size = new System.Drawing.Size(106, 46);
             this.localisationTxtBox.TabIndex = 0;
             this.localisationTxtBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // bunifuThinButton21
+            // 
+            this.bunifuThinButton21.ActiveBorderThickness = 1;
+            this.bunifuThinButton21.ActiveCornerRadius = 20;
+            this.bunifuThinButton21.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.bunifuThinButton21.ActiveForecolor = System.Drawing.Color.White;
+            this.bunifuThinButton21.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.bunifuThinButton21.BackColor = System.Drawing.Color.GhostWhite;
+            this.bunifuThinButton21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuThinButton21.BackgroundImage")));
+            this.bunifuThinButton21.ButtonText = "ThinButton";
+            this.bunifuThinButton21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuThinButton21.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuThinButton21.ForeColor = System.Drawing.Color.SeaGreen;
+            this.bunifuThinButton21.IdleBorderThickness = 1;
+            this.bunifuThinButton21.IdleCornerRadius = 20;
+            this.bunifuThinButton21.IdleFillColor = System.Drawing.Color.White;
+            this.bunifuThinButton21.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.bunifuThinButton21.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.bunifuThinButton21.Location = new System.Drawing.Point(617, 379);
+            this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(5);
+            this.bunifuThinButton21.Name = "bunifuThinButton21";
+            this.bunifuThinButton21.Size = new System.Drawing.Size(181, 41);
+            this.bunifuThinButton21.TabIndex = 3;
+            this.bunifuThinButton21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuThinButton21.Click += new System.EventHandler(this.BunifuThinButton21_Click);
+            // 
+            // cBoxPartners
+            // 
+            this.cBoxPartners.FormattingEnabled = true;
+            this.cBoxPartners.Location = new System.Drawing.Point(617, 109);
+            this.cBoxPartners.Name = "cBoxPartners";
+            this.cBoxPartners.Size = new System.Drawing.Size(181, 24);
+            this.cBoxPartners.TabIndex = 4;
             // 
             // confirmStandsModal
             // 
@@ -78,6 +115,8 @@ namespace Main
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.GhostWhite;
+            this.Controls.Add(this.cBoxPartners);
+            this.Controls.Add(this.bunifuThinButton21);
             this.Controls.Add(this.areaTxtBox);
             this.Controls.Add(this.localisationTxtBox);
             this.Name = "confirmStandsModal";
@@ -89,5 +128,7 @@ namespace Main
         #endregion
         private Bunifu.Framework.UI.BunifuMaterialTextbox areaTxtBox;
         private Bunifu.Framework.UI.BunifuMaterialTextbox localisationTxtBox;
+        private Bunifu.Framework.UI.BunifuThinButton2 bunifuThinButton21;
+        private ComboBox cBoxPartners;
     }
 }
--- Main/Main/confirmStandsModal.cs
+++ Main/Main/confirmStandsModal.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Main.DAO;
+using Main.LogicLayer;
 
 namespace Main
 {
@@ -15,17 +16,19 @@ namespace Main
     {
         private string location;
         private string area;
-        private int dbId; //Data base Card ID;
+        private int dbId; //Data base Stand ID;
+        private List<Partenaire> partners = new List<Partenaire>();
         public confirmStandsModal(string location, string area, int dbId)
         {
             this.location = location;
             this.area = area;
             this.dbId = dbId;
+
+            this.partners = DAOPartenaires.getAllPartners();
+
             InitializeComponent();
 
             this.initValues();
-
-            this.Focus();
 
             testInsert();
         }
@@ -40,7 +43,16 @@ namespace Main
             localisationTxtBox.Text = this.location;
             areaTxtBox.Text = this.area;
 
+            cBoxPartners.DataSource = partners;
+            cBoxPartners.DisplayMember = "Nom";
+
         }
 
+        private void BunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            Partenaire p = cBoxPartners.SelectedItem as Partenaire;
+            Console.WriteLine("Slected Item " + p.Id);
+            DAOStands.atributePartner(p.Id,dbId);
+        }
     }
 }
